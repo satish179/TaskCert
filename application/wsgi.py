702a7c5b6533e565d02e227b1c0672f83347acbb
Vercel Deploy <deploy@vercel.app>
2025-12-19 18:22:20 +0530

    Fix Vercel 500: Expose app variable for WSGI

--- application/wsgi.py
+++ application/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
 
 application = get_wsgi_application()
+
+# Alias for Vercel
+app = application
